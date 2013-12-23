a1fab8f9e0f5d7f155985976bf16f71c1c847b56
Tom Keays <tomkeays@gmail.com>
2013-12-23 12:46:57 -0500

    create calendar template page

---
+++ calendar.html.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Library Hours</title>
+
+	<style type="text/css" media="all">
+	#hours {
+		position:relative;
+		background-color: #fff;
+		border: 1px solid #e0e0e0;
+		font-family: Verdana, Arial, sans-serif;
+		font-size: 13px;
+	}	
+
+	.hoursbox {
+		background-color: #CFDAE6;
+	}	
+
+	.hours_header{
+		background-color: #fff;
+		text-align:center;
+		color: #000;
+		font-size: larger;
+	}	
+
+	.hours_subheader{
+		background-color: #8798AC;
+		text-align:center;
+		color: #fff;
+	}	
+
+	.hours_today {
+		background-color: #333;
+		color: #fff;
+	}
+	</style>
+
+</head>
+<body>
+
+	<?php echo $calendar; ?>
+	<br />
+
+</body>
+</html>
